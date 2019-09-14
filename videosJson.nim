0d1d350d8de48d612a674569fb52a612631925a3
Gabriel Lasso <gabriel.lasso@usp.br>
2019-09-14 09:28:36 -0300

    Add more videos

--- videosJson.nim
+++ videosJson.nim
@@ -18,7 +18,7 @@ const videosJsonStr* = """{
         {
             "id": "OO9j7rWNk9Y",
             "title": "10º Japan Matsuri 2019",
-            "description": "Kenka Yatai, Pikachu, Goen, Iroha, Shabondama, Irodori, Gekiryu, Jyuugosou monogatari"
+            "description": "Kenka Yatai, Pikachu, Go-en, Iroha, Shabondama, Irodori, Gekiryu, Jyuugosou monogatari"
         },
         {
             "id": "-X7mBGFsWNU",
@@ -37,12 +37,12 @@ const videosJsonStr* = """{
         },
         {
             "id": "IztKDK5iiDw",
-            "title": "VI Campeonato brasileiro 2009",
+            "title": "6º Campeonato brasileiro 2009",
             "description": "Go-en"
         },
         {
             "id": "7nBf_6lGyXk",
-            "title": "V Campeonato brasileiro 2008",
+            "title": "5º Campeonato brasileiro 2008",
             "description": "Wadatsumi"
         },
         {
@@ -54,6 +54,111 @@ const videosJsonStr* = """{
             "id": "yHO-FqF0UtA",
             "title": "Campeonato UCES 2013",
             "description": "Kizuki"
+        },
+        {
+            "id": "08GS3gPWk2M",
+            "title": "Campeonato brasileiro 2010 - Oodaiko",
+            "description": "Solo de oodaiko do Eiri"
+        },
+        {
+            "id": "jbXyJ2erGSY",
+            "title": "Ikoi no sono 2017",
+            "description": "Irodori, Shabondama, Fuuga tenshou, Omoide"
+        },
+        {
+            "id": "vVGl0XWC-HY",
+            "title": "7º Festival do japão da paraíba",
+            "description": "Furusatou no hibiki"
+        },
+        {
+            "id": "5ikL2dzWxzE",
+            "title": "8º Japan Matsuri 2017",
+            "description": "Kenka yatai"
+        },
+        {
+            "id": "k-YAMycpcdU",
+            "title": "5º Japan Matsuri 2014",
+            "description": "Fuuga tenshou"
+        },
+        {
+            "id": "HCEFaa8zkNs",
+            "title": "Osasco Todorokidaiko ao Vivo na Fundação Ema Klabin 2010",
+            "description": "Kenka yatai"
+        },
+        {
+            "id": "bP99slFincs",
+            "title": "46º Festival bunkyo 2011",
+            "description": "Shikisai"
+        },
+        {
+            "id": "DycWDS8Gwj0",
+            "title": "5º Japan Matsuri 2014",
+            "description": "Kizuki"
+        },
+        {
+            "id": "mKHaRhRsluM",
+            "title": "III Japan Matsuri 2012",
+            "description": "Yamato Gokoro"
+        },
+        {
+            "id": "BeU8H1j5YLk",
+            "title": "Apresentação Colégio Franciscano João XXIII 2016",
+            "description": "Irodori, Iroha, Shabondama, Pikachu, Fuuga tenshou"
+        },
+        {
+            "id": "MDmxVTvYzSk",
+            "title": "Apresentação na USP",
+            "description": "Kenka Yatai"
+        },
+        {
+            "id": "uNopg7nG5cw",
+            "title": "12ª Festa Japonesa de Sorocaba 2012",
+            "description": "Yamato gokoro"
+        },
+        {
+            "id": "hjt11pesTuU",
+            "title": "5º Japan Matsuri 2014",
+            "description": "Omoigakenai yuuyake, Iroha, Pikachu"
+        },
+        {
+            "id": "QedrzlLlAH0",
+            "title": "Campeonato UCES 2015",
+            "description": "Pikachu"
+        },
+        {
+            "id": "FmOZ9MOmzIU",
+            "title": "Apresentação Moa 2014",
+            "description": "Pikachu, Kenka yatai"
+        },
+        {
+            "id": "Pimm9cTGsyk",
+            "title": "8º Japan Matsuri 2017",
+            "description": "Irodori"
+        },
+        {
+            "id": "tUZdV9xQ9oA",
+            "title": "Apresentação em João Pessoa 2012",
+            "description": "Go-en"
+        },
+        {
+            "id": "8xezdnAazDI",
+            "title": "Treino geral Itapetininga 2016",
+            "description": "Seinansei monogatari"
+        },
+        {
+            "id": "k7aQrFImxoo",
+            "title": "5º Japan matsuri 2014",
+            "description": "Omoigakenai yuukake"
+        },
+        {
+            "id": "6iX_W2zuB_M",
+            "title": "Festival do Japão 2010",
+            "description": "Shikisai"
+        },
+        {
+            "id": "DByqM8oHaIA",
+            "title": "54º Undokai da acenbo 2018",
+            "description": "Fuuga tenshou, Irodori, Jyuugosou monogatari"
         }
     ]
 }"""
